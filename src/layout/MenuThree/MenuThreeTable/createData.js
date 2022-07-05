a0926f7a4efa422e86b09034a26e46d9ddfcf5b1
Oleg Marin <oleg.marin@riofi.ru>
2022-07-05 23:31:04 +0300

    linter createDatas and row

--- src/layout/MenuThree/MenuThreeTable/createData.js
+++ src/layout/MenuThree/MenuThreeTable/createData.js
@@ -1,24 +1,24 @@
 const createData = (name, calories, fat, carbs, protein, price) => {
-    return {
-      name,
-      calories,
-      fat,
-      carbs,
-      protein,
-      price,
-      history: [
-        {
-          date: '2020-01-05',
-          customerId: '11091700',
-          amount: 3,
-        },
-        {
-          date: '2020-01-02',
-          customerId: 'Anonymous',
-          amount: 1,
-        },
-      ],
-    };
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      {
+        date: '2020-01-05',
+        customerId: '11091700',
+        amount: 3,
+      },
+      {
+        date: '2020-01-02',
+        customerId: 'Anonymous',
+        amount: 1,
+      },
+    ],
   };
+};
 
-  export default createData
+export default createData;
