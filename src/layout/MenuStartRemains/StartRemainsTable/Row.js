91ff481d47630cee372e82895a4e3fb670252fe9
Oleg Marin <oleg.marin@riofi.ru>
2022-07-18 01:21:41 +0300

    add inputs in RemainsTables

--- src/layout/MenuStartRemains/StartRemainsTable/Row.js
+++ src/layout/MenuStartRemains/StartRemainsTable/Row.js
@@ -14,7 +14,13 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 const Row = (props) => {
   const { row } = props;
   const [open, setOpen] = React.useState(false);
-  
+    
+  const handleChangeQuantity = (e) => {
+    if(typeof props.onChangeQuantity === 'function'){
+      props.onChangeQuantity(e.target.value, props.index);
+    };
+  };
+
   return (
     <React.Fragment>
       <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
@@ -37,7 +43,11 @@ const Row = (props) => {
         <TableCell align="right">{row.protein}</TableCell>
         */}
         <TableCell align="right">
-          {row.quantity}
+          <input
+            value={row.quantity}
+            type="number"
+            onChange={handleChangeQuantity}
+          ></input>
         </TableCell>
 
       </TableRow>
--- src/layout/MenuStartRemains/index.jsx
+++ src/layout/MenuStartRemains/index.jsx
@@ -6,10 +6,34 @@ import style from './index.module.css';
 
 
 const MenuStartRemains = () => {
+  const context = useContext(MyContext);
+  const [invoice, setInvoice] = useState([...context.remains]);
+
+  const onChangeQuantity = (quantityValue, quantityIndex) => {
+    let newReturnInvoice = [...invoice];
+    let rowIndex = +quantityIndex;
+    (typeof newReturnInvoice[rowIndex]?.quantity === 'number' || typeof newReturnInvoice[rowIndex]?.quantity === 'string')
+      ? newReturnInvoice[rowIndex].quantity = +quantityValue : null;
+    setInvoice(newReturnInvoice);
+  };
+
+
+  const setServerRemains = () => {
+    if (!invoice.length){
+      let newInvoice = [...context.remains];
+      setInvoice(newInvoice);
+    }
+  };
+
+  useEffect(()=>{
+    setServerRemains();
+  },[context.remains.length]);
 
   return(
     <div className={style.table}>
-      <StartRemainsTable/>
+      <StartRemainsTable
+        invoice={invoice}
+        onChangeQuantity={onChangeQuantity}/>
     </div>
 
   );
